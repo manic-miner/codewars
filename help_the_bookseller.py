fb0057747d37d1f3cb37b8a0d7e37da33c8c84f7
tomek <thetomk@gmail.com>
2023-02-23 10:52:30 +0100

    Finished Help the bookseller

--- help_the_bookseller.py
+++ help_the_bookseller.py
@@ -1,20 +1,23 @@
-list_of_art = ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"]
-list_of_cat = ["A", "B", "C", "D"]
+list_of_art = ["ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"]
+list_of_cat = ["A", "B"]
 
 
 def stock_list(list_of_art, list_of_cat):
     result = {}
+    for letter in list_of_cat:
+        result[letter] = 0
+        print(result)
     for item in list_of_art:
         key_letter = item[0]
         books_count = item[-3:]
 
         if key_letter in list_of_cat:
-            result[key_letter] = int(books_count)
+            result[key_letter] += int(books_count)
             print(result)
-        elif key_letter == "":
-            return ""
-        else:
-            result[key_letter] = 0
+
+    if not list_of_art:
+        return ""
+
     total_string = ""
     for key, value in result.items():
         string = f"({key} : {value})"
