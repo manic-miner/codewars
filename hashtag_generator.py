5246caea0bf9b16240a5f6133f0b7aff7e70e512
tomek <thetomk@gmail.com>
2023-02-27 08:51:33 +0100

    Fixed output length

--- hashtag_generator.py
+++ hashtag_generator.py
@@ -1,6 +1,4 @@
 def generate_hashtag(s):
-    if 0 < len(s) <= 140:
-        return "#" + "".join(s.title().replace(" ", "").strip())
-    else:
-        return False
+    output = "#" + "".join(s.title().replace(" ", "").strip())
+    return False if (len(s) <= 0 or len(output) > 140) else output
 
